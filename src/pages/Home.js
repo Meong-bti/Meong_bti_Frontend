c53541aa3981afa095dadaf3f1b66de6dea74624
happyjino <wlsgh1128@naver.com>
2023-03-04 19:32:00 +0900

    fix. Reset.css - #root 주석

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const goLink = () => {
-        navigate('/DogSelect', {
-                state: {
-                    dInfo: "미미",
-                }
-            }
-        );
-    };
-    return (
-        <div id="Main">
-            <div className="main_box" onClick={goLink}>
-                <div className="main_logo">멍BTI LOGO</div>
-                <div className="main_img_box"></div>
-            </div>
-        </div>
-    );
+  const goLink = () => {
+    navigate("/DogSelect", {
+      state: {
+        dInfo: "미미",
+      },
+    });
+  };
+
+  return (
+    <div id="Main">
+      <div className="main_box" onClick={goLink}>
+        <div className="main_logo">멍BTI LOGO</div>
+        <div className="main_img_box"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
