3837244e95c3630af2c97cfb7e89da4a807f21be
happyjino <wlsgh1128@naver.com>
2023-08-29 23:52:48 +0900

    로그아웃 토큰 삭제 및 기타 수정

--- src/api/member/logout.js
+++ src/api/member/logout.js
@@ -2,35 +2,35 @@ import { domain } from "../domain"
 
 export const logoutMember = async ({navigate, loginUpdate}) => {
 
-  const token = localStorage.getItem('token');
+  localStorage.removeItem('token');
+  loginUpdate();
+  navigate('/');
 
-  const response = await fetch(`${domain}/auth/logout`, {
-    method: 'GET',
-    // headers: {
-    //   'Authorization': `Bearer ${token}`
-    // }
-  });
+  // const response = await fetch(`${domain}/auth/logout`, {
+  //   method: 'GET',
+  //   // headers: {
+  //   //   'Authorization': `Bearer ${token}`
+  //   // }
+  // });
 
-  if (response.ok) {
-    localStorage.removeItem('token');
-    loginUpdate();
-    navigate('/');
-  } else if(response.status === 401) {
-    alert('logout 실패')
-    alert('토큰 재발급')
-    const response2 = await fetch(`${domain}/auth/reissueToken`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
+  // if (response.ok) {
+  //   localStorage.removeItem('token');
+  //   loginUpdate();
+  //   navigate('/');
+  // } else if(response.status === 401) {
+  //   const response2 = await fetch(`${domain}/auth/reissueToken`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`
+  //     }
+  //   });
 
-    if (response2.ok) {
-      console.log("재발급 성공");
-      console.log(response2.accessToken)
-    } else {
-      console.log("재발급 실패")
-      console.log(response2.status);
-    }
-  }
+  //   if (response2.ok) {
+  //     console.log("재발급 성공");
+  //     console.log(response2.accessToken)
+  //   } else {
+  //     console.log("재발급 실패")
+  //     console.log(response2.status);
+  //   }
+  // }
 }
