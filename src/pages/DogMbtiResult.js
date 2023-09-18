6c15e13d1f19c0bd3865e23c7cd62aeec12d15cf
happyjino <wlsgh1128@naver.com>
2023-09-19 01:08:47 +0900

    공유하기 테스트 6

--- src/pages/DogMbtiResult.js
+++ src/pages/DogMbtiResult.js
@@ -57,16 +57,73 @@ const DogMbtiResult = () => {
       if (!kakao.isInitialized()) {
         kakao.init("cc5ebe066875a3d0c3214fc264f06811");
       }
-
       kakao.Share.sendDefault({
-        objectType: 'text',
-        text: 'DBTI 테스트 결과를 확인해봐요',
-        link: {
-          webUrl: url,
-          mobileWebUrl: url,
+        objectType: 'feed',
+        content: {
+          title: '오늘의 디저트',
+          description: '아메리카노, 빵, 케익',
+          imageUrl:
+            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
         },
-        // button_title: '바로 확인',
+        itemContent: {
+          profileText: 'Kakao',
+          profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageText: 'Cheese cake',
+          titleImageCategory: 'Cake',
+          items: [
+            {
+              item: 'Cake1',
+              itemOp: '1000원',
+            },
+            {
+              item: 'Cake2',
+              itemOp: '2000원',
+            },
+            {
+              item: 'Cake3',
+              itemOp: '3000원',
+            }
+          ],
+          sum: '총 결제금액',
+          sumOp: '15000원',
+        },
+        social: {
+          likeCount: 10,
+          commentCount: 20,
+          sharedCount: 30,
+        },
+        buttons: [
+          {
+            title: '웹으로 이동',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+          {
+            title: '앱으로 이동',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+        ],
       });
+
+      // kakao.Share.sendDefault({
+      //   objectType: 'text',
+      //   text: 'DBTI 테스트 결과를 확인해봐요',
+      //   link: {
+      //     webUrl: url,
+      //     mobileWebUrl: url,
+      //   },
+      //   // button_title: '바로 확인',
+      // });
     }
   }
 
